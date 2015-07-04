310508769fd5d53c02f8425f9d2fd7b3b5451a62
hekt <hektorg@gmail.com>
2015-07-04 16:02:46 +0900

    remove file

--- src/Connect.hs
+++
@@ -1,8 +0,0 @@
-module DataSource
-       ( connect
-       ) where
-
-import Database.HDBC.PostgreSQL (connectPostgreSQL, Connection)
-
-connect :: IO Connection
-connect = connectPostgreSQL "dbname=testdb"
